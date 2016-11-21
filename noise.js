fe6de875d55c3fd4504943e70ca03add92ce2d21
Nina Mutty <ninamutty@gmail.com>
2016-11-21 15:05:22 -0800

    refactored so much shorter and can play same note back to back

--- noise.js
+++ noise.js
@@ -2,59 +2,23 @@ $(document).ready( function() {
   // your code here
   // Target the audio tag
 
-  $('.instrument').on('click', 'button', function(note = null) {
-    if ($(this).hasClass('c') == true) {
-      note = document.getElementById('cAudio');
-    } else if ($(this).hasClass('d') == true) {
-      note = document.getElementById('dAudio');
-    } else if ($(this).hasClass('e') == true) {
-      note = document.getElementById('eAudio');
-    } else if ($(this).hasClass('f') == true) {
-      note = document.getElementById('fAudio');
-    } else if ($(this).hasClass('g') == true) {
-      note = document.getElementById('gAudio');
-    } else if ($(this).hasClass('a') == true) {
-      note = document.getElementById('aAudio');
-    } else if ($(this).hasClass('b') == true) {
-      note = document.getElementById('bAudio');
-    }
-    return note.play();
+  $('.instrument').on('click', 'button', function() {
+    var myclass = this.className.slice(-1);
+    id = myclass + 'Audio';
+    var note = document.getElementById(id);
+
+    note.play();
+    note.currentTime = 0;
   });
 
 
   $('body').keydown(function(event) {
-    var note = null;
-    switch (event.key) {
-      case 'c':
-      case 'C':
-        note = document.getElementById('cAudio');
-        break;
-      case 'd':
-      case 'D':
-        note = document.getElementById('dAudio');
-        break;
-      case 'e':
-      case 'E':
-        note = document.getElementById('eAudio');
-        break;
-      case 'f':
-      case 'F':
-        note = document.getElementById('fAudio');
-        break;
-      case 'g':
-      case 'G':
-        note = document.getElementById('gAudio');
-        break;
-      case 'a':
-      case 'A':
-        note = document.getElementById('aAudio');
-        break;
-      case 'b':
-      case 'B':
-        note = document.getElementById('bAudio');
-        break;
-    }
-    return note.play();
+    var myclass = event.key.toLowerCase();
+    id = myclass + 'Audio';
+    var note = document.getElementById(id);
+
+    note.play();
+    note.currentTime = 0;
   });
 
 });
