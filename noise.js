50b0c8cb80e2074a30561934855aed5d8a4dd811
Nina Mutty <ninamutty@gmail.com>
2016-11-21 14:44:04 -0800

    will refactor

--- noise.js
+++ noise.js
@@ -2,29 +2,25 @@ $(document).ready( function() {
   // your code here
   // Target the audio tag
 
-  $('.instrument').on('click', 'button', function() {
-    var note = null;
+  $('.instrument').on('click', 'button', function(note = null) {
     if ($(this).hasClass('c') == true) {
       note = document.getElementById('cAudio');
     } else if ($(this).hasClass('d') == true) {
       note = document.getElementById('dAudio');
     } else if ($(this).hasClass('e') == true) {
       note = document.getElementById('eAudio');
-
     } else if ($(this).hasClass('f') == true) {
       note = document.getElementById('fAudio');
-
     } else if ($(this).hasClass('g') == true) {
       note = document.getElementById('gAudio');
-
     } else if ($(this).hasClass('a') == true) {
       note = document.getElementById('aAudio');
-
     } else if ($(this).hasClass('b') == true) {
       note = document.getElementById('bAudio');
     }
     return note.play();
   });
+
 
   $('body').keydown(function(event) {
     var note = null;
@@ -62,34 +58,3 @@ $(document).ready( function() {
   });
 
 });
-
-
-
-// $('.c').click(function() {
-//   var c = document.getElementById('cAudio')
-//   c.play();
-// });
-// $('.d').click(function() {
-//   var d = document.getElementById('dAudio')
-//   d.play();
-// });
-// $('.e').click(function() {
-//   var e = document.getElementById('eAudio')
-//   e.play();
-// });
-// $('.f').click(function() {
-//   var f = document.getElementById('fAudio')
-//   f.play();
-// });
-// $('.g').click(function() {
-//   var g = document.getElementById('gAudio')
-//   g.play();
-// });
-// $('.a').click(function() {
-//   var a = document.getElementById('aAudio')
-//   a.play();
-// });
-// $('.b').click(function() {
-//   var b = document.getElementById('bAudio')
-//   b.play();
-// });
