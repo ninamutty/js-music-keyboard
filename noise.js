f36c3531f033230aa6cf40e46799267fa307fd61
Nina Mutty <ninamutty@gmail.com>
2016-11-22 09:21:15 -0800

    added jquery

--- noise.js
+++ noise.js
@@ -1,24 +1,33 @@
 $(document).ready( function() {
-  // your code here
-  // Target the audio tag
-
   $('.instrument').on('click', 'button', function() {
     var myclass = this.className.slice(-1);
-    id = myclass + 'Audio';
-    var note = document.getElementById(id);
 
-    note.play();
-    note.currentTime = 0;
+    // Without jQuery
+    // var id = myclass + 'Audio';
+    // var note = document.getElementById(id);
+    // note.play();
+    // note.currentTime = 0;
+
+    // With jQuery
+    var id = "#" + myclass + 'Audio';
+    $(id)[0].play();
+    $(id)[0].currentTime = 0;
   });
 
 
   $('body').keydown(function(event) {
     var myclass = event.key.toLowerCase();
-    id = myclass + 'Audio';
-    var note = document.getElementById(id);
 
-    note.play();
-    note.currentTime = 0;
+    // Without jQuery
+    // id = myclass + 'Audio';
+    // var note = document.getElementById(id);
+    // note.play();
+    // note.currentTime = 0;
+
+    // With jQuery
+    var id = "#" + myclass + 'Audio';
+    $(id)[0].play();
+    $(id)[0].currentTime = 0;
   });
 
 });
