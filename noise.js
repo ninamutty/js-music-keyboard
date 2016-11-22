d4232184adc29759302843a6a35285d7a94700a7
Nina Mutty <ninamutty@gmail.com>
2016-11-22 09:29:07 -0800

    cleaned up

--- noise.js
+++ noise.js
@@ -2,13 +2,6 @@ $(document).ready( function() {
   $('.instrument').on('click', 'button', function() {
     var myclass = this.className.slice(-1);
 
-    // Without jQuery
-    // var id = myclass + 'Audio';
-    // var note = document.getElementById(id);
-    // note.play();
-    // note.currentTime = 0;
-
-    // With jQuery
     var id = "#" + myclass + 'Audio';
     $(id)[0].play();
     $(id)[0].currentTime = 0;
@@ -18,16 +11,30 @@ $(document).ready( function() {
   $('body').keydown(function(event) {
     var myclass = event.key.toLowerCase();
 
-    // Without jQuery
-    // id = myclass + 'Audio';
-    // var note = document.getElementById(id);
-    // note.play();
-    // note.currentTime = 0;
-
-    // With jQuery
     var id = "#" + myclass + 'Audio';
     $(id)[0].play();
     $(id)[0].currentTime = 0;
   });
 
 });
+
+
+
+///////// Without jQuery /////////////
+
+// $(document).ready( function() {
+//   $('.instrument').on('click', 'button', function() {
+//     var myclass = this.className.slice(-1);
+//     // var id = myclass + 'Audio';
+//     // var note = document.getElementById(id);
+//     // note.play();
+//     // note.currentTime = 0;
+//   });
+//   $('body').keydown(function(event) {
+//     var myclass = event.key.toLowerCase();
+//     // id = myclass + 'Audio';
+//     // var note = document.getElementById(id);
+//     // note.play();
+//     // note.currentTime = 0;
+//   });
+// });
